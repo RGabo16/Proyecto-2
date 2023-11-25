23e19cfa7f34349f7fbf00fc4e24a80202c9c73a
Ronald Salinas <rgabo1603@gmail.com>
2023-11-25 15:53:28 -0500

    existe

--- Proyecto2/src/EDD/Document.java
+++ Proyecto2/src/EDD/Document.java
@@ -211,4 +211,24 @@ public class Document {
         }   
         return doc;
     }
+     public boolean existe(Document rdoc,Document doc){
+//        Document document = (Document) nodo.getElement();
+        if (rdoc == null){
+            return false;
+        }
+        if (rdoc.getSize() == doc.getSize()){
+            return true;
+        }
+        else if (doc.getSize() < doc.getSize()){
+            return existe(doc.getDerecha(), doc);
+        }else{
+            return existe(doc.getIzquierda(), doc);
+        }
+    }
+    public void eliminarmin(Document rdoc){
+        rdoc.getIzquierda();
+            
+        
+        
+    }
 }
